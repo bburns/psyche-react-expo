f37901937082bbce27f491eadd4050d6cfbae68f
Brian Burns <bburns.km@gmail.com>
2023-12-29 17:53:46 -0600

    wip

---
+++ src/styles.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native"
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  text: {
+    // fontFamily: "Cochin"
+    fontFamily: "mayqueen"
+  },
+  headerText: {
+    fontSize: 20,
+    fontWeight: "bold"
+  }
+})
+
+export default styles
