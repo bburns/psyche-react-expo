29ac8e00e32eee835c9ca12a544df3caedce9070
Brian Burns <bburns.km@gmail.com>
2023-12-30 08:55:22 -0600

    wip

--- App.js
+++ App.js
@@ -1,20 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar"
+import { View } from "react-native"
+import styles from "./src/styles"
+import { PsycheText, PsycheHeaderText } from "./src/components"
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <PsycheHeaderText>Psyche</PsycheHeaderText>
+      <PsycheText>Open up App.js to start working on your app!</PsycheText>
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
