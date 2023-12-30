11d2cc0a07584db0d3b25112bc3e8e2b3b8fb49f
Brian Burns <bburns.km@gmail.com>
2023-12-30 15:39:38 -0600

    wip

--- App.js
+++ App.js
@@ -4,11 +4,13 @@
 // import styles from "./src/styles"
 
 // GluestackUIProvider provides aliases and tokens throughout your app, using React's Context API.
-import { GluestackUIProvider, Box, Heading, Text } from "@gluestack-ui/themed"
+import { GluestackUIProvider, Box, Heading, Text, View } from "@gluestack-ui/themed"
 import { config as defaultConfig } from "@gluestack-ui/config"
 import { createConfig } from "@gluestack-ui/themed"
-import { ImageBackground } from "react-native"
-const image = { uri: "https://legacy.reactjs.org/logo-og.png" }
+import { StyleSheet, ImageBackground } from "react-native"
+
+const image = require("./src/assets/backgrounds/pexels-min-an-920534.jpg") // local image
+// const image = { uri: "https://foo.com/assets/backgrounds/pexels-min-an-920534.jpg" } // remote image
 
 const config = createConfig({
   ...defaultConfig,
@@ -22,19 +24,42 @@ const config = createConfig({
   }
 })
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "column"
+  },
+  image: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+    justifyContent: "center"
+  },
+  heading: {
+    color: "white",
+    bold: true
+  },
+  text: {
+    color: "lightgray"
+  }
+})
+
 export default function App() {
   return (
     <GluestackUIProvider config={config}>
-      {/* <Box width="100%" justifyContent="center" alignItems="center" height="100%"> */}
-      {/* <ImageBackground source={image} resizeMode="cover" style={image}> */}
-      <Box width="100%" justifyContent="center" alignItems="center" bg="$primary500">
-        <Heading bold color="$light100">
-          Psyche
-        </Heading>
-        <Text color="$light300">An open-source mental health app</Text>
+      {/* <Box width="100%" justifyContent="center" alignItems="center" bg="$primary500">
+      </Box> */}
+      <Box style={styles.container}>
+        <ImageBackground source={image} style={styles.image}>
+          {/* <Text style={styles.text}>Elements</Text>
+          <Text style={styles.text}>in Front of</Text>
+          <Text style={styles.text}>Background</Text> */}
+          <Heading style={styles.heading}>Psyche</Heading>
+          <Text style={styles.text}>An open-source mental health app</Text>
+        </ImageBackground>
       </Box>
-      {/* </ImageBackground> */}
-      {/* </Box> */}
     </GluestackUIProvider>
   )
 }
